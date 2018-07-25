9b832bb761d55b5f40ff5d7eebdbcc55f38dc79b
longhao666 <305902464@qq.com>
2018-07-25 19:45:08 +0800

    写了一些关于算法的介绍

--- main.cpp
+++ main.cpp
@@ -6,10 +6,23 @@
 
 using namespace std;
 
+void fun(char str[100]) {
+
+    std::cout << sizeof(int *) << std::endl;
+    std::cout << sizeof(char *) << std::endl;
+    std::cout << sizeof(str) << std::endl;
+
+    char s[100];
+    std::cout << sizeof(s) << std::endl;
+
+}
+
 
 int main()
 {
     cout << "Hello World!" << endl;
+
+    fun("fffff");
 
     lh::paiXu();
 
